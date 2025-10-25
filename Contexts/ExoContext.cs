ac86d73dc30ee31ec29d18fc6d053d52620560db
MaryScript <mariap.sousa@undf.edu.br>
2025-10-25 14:19:36 -0300

    Versão inicial

---
+++ Contexts/ExoContext.cs
@@ -0,0 +1,17 @@
+using Exo.WebApi.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Data.SqlClient;
+namespace Exo.WebApi.Contexts
+{
+public class ExoContext : DbContext
+{
+public ExoContext()
+{
+}
+public ExoContext(DbContextOptions<ExoContext> options) :
+base(options)
+{
+}
+public DbSet<Projeto> Projetos { get; set; }
+}
+}
